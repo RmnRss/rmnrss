c07e191555ff643586820c42b84bc4cef16c0c55
Romain Rousseau <romain.rousseau@weenat.com>
2024-05-04 17:47:31 +0200

    clean up for reset

--- components/nav/Header.tsx
+++ components/nav/Header.tsx
@@ -1,43 +1,20 @@
 import Link from "next/link"
-import { FC, useState } from "react"
-import Icon from "../Icon"
-import Drawer from "./Drawer"
+import { FC } from "react"
 
 const Header: FC = () => {
-  const [showDrawer, setShowDrawer] = useState(false)
-
-  const toggleDrawer = () => {
-    setShowDrawer(!showDrawer)
-  }
-
-  const closeDrawer = () => {
-    setShowDrawer(false)
-  }
-
   return (
     <>
       <header className={`w-full p-4 flex justify-center align-middle`}>
-        <nav className="w-full flex align-middle justify-between max-w-7xl">
-          <Link
-            passHref
-            href={"/"}
-            className="pr"
-            target="_self"
-            onClick={() => closeDrawer()}
-          >
+        <nav className="flex justify-self-center justify-center border rounded-full border-white max-w-4xl gap-2 p-3">
+          <Link passHref href={"/"} className="pr font-normal" target="_self">
             rmnrss.io
           </Link>
 
-          <Icon
-            name={showDrawer ? "close" : "menu"}
-            size={24}
-            color={"lightBlue"}
-            onClick={() => toggleDrawer()}
-          />
+          {/* <Link passHref href={"/"} className="pr font-normal" target="_self">
+            Gallery
+          </Link> */}
         </nav>
       </header>
-
-      <Drawer open={showDrawer} toggleDrawer={closeDrawer} />
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,49 @@
-import Keyboard from "../components/Keyboard"
-import ProcrastinationCube from "../components/ProcrastinationCube"
 import SEO from "../components/seo"
 import FullscreenLayout from "../layouts/FullscreenLayout"
 
 function IndexPage() {
   return (
-    <body>
+    <>
       <SEO title="rmnrss.io" />
       <FullscreenLayout title="rmnrss.io">
-        <div className="w-full h-full flex flex-col align-middle justify-start p-4">
+        <div className="w-full h-full flex flex-col justify-center items-center p-4">
           <h1 className="text-4xl font-bold text-lightBlue text-center mb-8">
             Hello there!
           </h1>
-          <b className="text-2xl font-medium text-center  mb-8">
-            Seems like this is still a work in progress...
-          </b>
-          <p className="text-lg text-lightBlue text-center">
-            While you wait feel free to play with the procrastination cub over
-            there
+          <p className="text-lg text-lightBlue text-center max-w-lg font-normal ">
+            I'm Romain, I'm a 28 years old design engineer, currently working at
+            Weenat!
+          </p>
+          <p className="text-lg text-lightBlue text-center max-w-lg font-normal">
+            I might throw random content or stuff I'm passionate about one day.
+          </p>
+          <a
+            href="https://github.com/RmnRss"
+            target="_blank"
+            className="text-lg text-lightBlue text-center max-w-lg font-normal"
+          >
+            Github
+          </a>
+          <a
+            href="https://www.linkedin.com/in/rmnrss/"
+            target="_blank"
+            className="text-lg text-lightBlue text-center max-w-lg font-normal"
+          >
+            Linkedin
+          </a>
+          {/* <p className="text-lg text-lightBlue text-center max-w-lg font-normal">
+            Until that day arrives, you can play with our good friend the
+            procrastination cube below!
           </p>
           <div className="m-auto">
             <ProcrastinationCube />
           </div>
           <div className="mt-auto align-bottom">
             <Keyboard />
-          </div>
+          </div> */}
         </div>
       </FullscreenLayout>
-    </body>
+    </>
   )
 }
 

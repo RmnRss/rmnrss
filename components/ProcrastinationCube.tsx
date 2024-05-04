724e37ccc8a9674cd8cb8c8353895c4eaaaa0860
Romain Rousseau <romain.rousseau@weenat.com>
2024-05-04 17:25:45 +0200

    removed smile from cube

--- components/ProcrastinationCube.tsx
+++ components/ProcrastinationCube.tsx
@@ -19,12 +19,6 @@ const ProcrastinationCube: FC<ProcrastinationCubeProps> = ({
       <rect width="32" height="32" rx="8" fill={colors[color]} />
       <circle cx="10" cy="12" r="2" fill={colors.light} />
       <circle cx="23" cy="12" r="2" fill={colors.light} />
-      <path
-        d="M18.3346 25.3709C21.1681 25.8981 22.8237 23.4333 22.8237 20.8818"
-        stroke={colors.light}
-        strokeWidth="3"
-        strokeLinecap="round"
-      />
     </svg>
   )
 }
